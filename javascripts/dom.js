235838c298864626915cde47338692975b7ca2a9
Dre mf Randaci <drerandaci@gmail.com>
2017-10-10 21:28:32 -0500

    added cat snacks to each dino

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const buildDomString = (dinosaur) => {
-    console.log('buildDom entered', dinosaur);
+    console.log('dinosaurs in buildDom', dinosaur);
     let domString = '';
     dinosaur.forEach((dino) => {
         domString += `<div class='col-md-3'>`;
-        domString +=    `<h1>${dino.type}</h1>`;
+        domString +=    `<h3>${dino.type}</h3>`;
+        // domString +=    `<h4>${dino.snacks.name}</h4>`;
         domString += `</div>`;
     });
     writeToDom(domString);
